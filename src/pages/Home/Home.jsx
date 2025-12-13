15d0aa6c2568e59bcb8d7b2cb2ea90ddd3f7196a
Anuj Prakash Dixit <anujprakashdixit@gmail.com>
2025-12-13 22:15:12 +0530

    Created Home Page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,25 @@ import './Home.css'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className="hero">
+        <h1>Largest <br/> Crypto Marketplace</h1>
+        <p>Welcome to the world's largest cryptocurrency marketplace. Sign up to explore more about cryptos.</p>
+        <form>
+          <input type="text" placeholder='Search coin..'/>
+          <button type='submit'>Search</button>
+        </form>
+      </div>
+      <div className="crypto-table">
+        <div className="table-layout">
+          <p>#</p>
+          <p>Coins</p>
+          <p>Price</p>
+          <p style={{textAlign:"center"}}>24hr Change</p>
+          <p className='market-cap'>Market Cap</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
